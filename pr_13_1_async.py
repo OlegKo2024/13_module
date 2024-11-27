dafc33a814715636d92a2dbb247f3228dbdcc486
OlegKo2024 <4oki66@gmail.com>
2024-11-28 00:31:18 +0300

    Add files via upload

--- pr_13_1_async.py
+++ pr_13_1_async.py
@@ -6,7 +6,7 @@ print('Асинхронные силачи')
 
 async def start_strongman(name, power, balls=5):
     print(f'Силач {name} начал соревнования')
-    for i in range(1, 6):
+    for i in range(1, balls + 1):
         print(f'Силач {name} поднял {i}')
         await asyncio.sleep(balls / power)
     print(f'Силач {name} закончил соревнования')
@@ -23,5 +23,29 @@ async def start_tournament():
 
 start = time.time()
 asyncio.run(start_tournament())
+end = time.time()
+print(f'Working = {round(end - start, 2)}')
+
+
+print('Асинхронные силачи списком')
+
+async def start_strongman_list(name, power, balls=5):
+    print(f'Силач {name} начал соревнования')
+    for i in range(1, balls+1):
+        print(f'Силач {name} поднял {i}')
+        await asyncio.sleep(balls / power)
+    print(f'Силач {name} закончил соревнования')
+
+async def main(power_men_list):
+    tasks = []
+    for name, power in power_men_list:
+        power_man = asyncio.create_task(start_strongman_list(name, power))
+        tasks.append(power_man)
+
+    await asyncio.gather(*tasks)  # Дожидаемся завершения всех задач
+
+start = time.time()
+power_men_list = [('Pasha', 3), ('Denis', 4), ('Apollon', 5)]
+asyncio.run(main(power_men_list))
 end = time.time()
 print(f'Working = {round(end - start, 2)}')
