97361ce57bf1763dcb9b3fb835eb104cda95c761
OlegKo2024 <4oki66@gmail.com>
2024-12-07 13:22:48 +0300

    Add files via upload

---
+++ pr_13.3_bot_answer.py
@@ -0,0 +1,43 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import Router
+import asyncio
+import dotenv
+import os
+
+dotenv.load_dotenv()
+api = os.getenv("BOT_TOKEN")
+bot = Bot(token=api)
+storage = MemoryStorage()
+router = Router()
+dp = Dispatcher(storage=storage)
+dp.include_router(router)
+
+
+@router.message(Command("start"))
+async def start_com(message: Message):
+    # print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+# @router.message(F.text.in_(['Urban', 'ff']))
+# async def urban_messages(message: Message):
+#     print('Urban message:', message.text)
+#     await message.reply('Получено кодовое сообщение: ' + message.text)
+
+
+@router.message()
+async def any_messages(message: Message):
+    # print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+async def main():
+    print("Updates were skipped successfully.")
+    await dp.start_polling(bot, skip_updates=True)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
